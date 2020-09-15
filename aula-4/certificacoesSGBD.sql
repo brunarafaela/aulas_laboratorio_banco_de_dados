a5757d19d347e0261bd150452f207a0d17da8135
brunarafaela <brunarafaelav@outlook.com>
2020-09-15 18:10:53 -0300

    Update certificacoesSGBD.sql

--- aula-4/certificacoesSGBD.sql
+++ aula-4/certificacoesSGBD.sql
@@ -712,6 +712,18 @@ Aula 4 - SQL - DDL e DML
 INNER JOIN E Funcoes de agregacao -- Group by
 ******************************************************************/
 
+select * from aluno;
+select * from matricula;
+select * from aula;
+select * from certificacao;
+select * from empresa_certificacao;
+select * from softw_uso_curso;
+select * from fabricante_softw;
+select * from turma;
+select * from curso;
+select * from participacao aula;
+select * from software;
+select * from uso_softwares_curso;
 
 --17- Alunos matriculados em cursos que a empresa certificadora é fabricante dos softwares usados no curso
 SELECT a.nome_aluno, c.nome_curso, fsw.nome_fabr
@@ -738,7 +750,6 @@ SELECT COUNT(*) As Qtde_linhas,
 	MIN(c.carga_hora_curso) AS Menor
 FROM curso c;	
 
-
 --19- por certificacao
 SELECT COUNT(*) FROM matricula;
 SELECT * FROM CURSO;
@@ -760,3 +771,68 @@ WHERE (mt.num_turma = t.num_turma AND mt.id_curso = t.id_curso)
 AND t.id_curso = c.id_curso
 AND mt.dt_hora_matr >= current_timestamp - INTERVAL '12' MONTH
 GROUP BY c.nome_curso;
+
+--21 - IDEM, AGORA POR MES
+UPDATE matricula SET dt_hora_matr = dt_hora_matr - INTERVAL '1' MONTH
+WHERE MOD(num_matricula, 2) =1;
+
+SELECT c.nome_curso, EXTRACT(MONTH FROM mt.dt_hora_matr) AS Mes,
+SUM(mt.vl_pago) AS "Valor total por curso", COUNT(*) AS Matriculados
+FROM curso c, matricula mt, turma t
+WHERE (mt.num_turma = t.num_turma AND mt.id_curso = t.id_curso)
+AND t.id_curso = c.id_curso
+AND mt.dt_hora_matr >= current_timestamp - INTERVAL '12' MONTH
+GROUP BY c.nome_curso, EXTRACT(MONTH FROM mt.dt_hora_matr), c.nome_curso
+ORDER BY 1,2;
+--Regra do grup by: as colunas que nao tem funcao de grupo, sao as colunas que tem que aparecer na clausula group by
+
+
+--22 - idem mas que tenha arrecadado mais de 750
+SELECT c.nome_curso, EXTRACT(MONTH FROM mt.dt_hora_matr) AS Mes,
+SUM(mt.vl_pago) AS "Valor total por curso", COUNT(*) AS Matriculados
+FROM curso c, matricula mt, turma t
+WHERE (mt.num_turma = t.num_turma AND mt.id_curso = t.id_curso) --condicao para linha, antes do grupo by
+AND t.id_curso = c.id_curso
+AND mt.dt_hora_matr >= current_timestamp - INTERVAL '12' MONTH
+GROUP BY c.nome_curso, EXTRACT(MONTH FROM mt.dt_hora_matr), c.nome_curso
+HAVING SUM(mt.vl_pago) > 750 --where ou condicao para grupo, depois do grupo by 
+--HAVING COUNT(*)>1
+ORDER BY 1,2;
+
+
+--23- mostrar a media de idade dos alunos por sexo e curso que tem mais de 1 matriculado
+SELECT c.nome_curso, a.sexo_aluno, AVG((current_date - a.dt_nascto_aluno)/365.25) AS Media_Idade
+FROM aluno a join matricula mt
+	ON (a.id_aluno = mt.id_aluno)
+	JOIN turma t
+	ON (mt.num_turma = t.num_turma AND mt.id_curso = t.id_curso)
+	JOIN curso c
+	ON (t.id_curso = c.id_curso)
+GROUP BY c.nome_curso, a.sexo_aluno
+HAVING COUNT(mt.id_aluno) >1;
+
+
+--24- mostre a quantidade de alunos matriculados em cada turma
+SELECT t.num_turma, t.id_curso, COUNT(mt.id_aluno) AS Matriculados
+FROM turma t INNER JOIN matricula mt
+	ON (t.num_turma = mt.num_turma)
+	WHERE mt.situacao_matr != 'CANCELADA'
+	GROUP BY t.num_turma, t.id_curso;
+--somente por curso
+SELECT t.id_curso, COUNT(mt.id_aluno) AS Matriculados
+FROM turma t INNER JOIN matricula mt
+	ON (t.num_turma = mt.num_turma)
+	WHERE mt.situacao_matr != 'CANCELADA'
+	GROUP BY t.id_curso;
+
+--25 - idem com menos de 10 e para cada certificacao
+SELECT  t.num_turma, t.id_curso, ce.nome_cert, COUNT(mt.id_aluno) AS Matriculados
+FROM turma t INNER JOIN matricula mt
+	ON (t.num_turma = mt.num_turma)
+	INNER JOIN curso c
+	ON  (t.id_curso = c.id_curso)
+	INNER JOIN certificacao ce 
+	ON (c.id_cert = ce.id_cert)
+WHERE mt.situacao_matr != 'CANCELADA'
+GROUP BY t.num_turma, t.id_curso, ce.nome_cert
+HAVING COUNT(mt.id_aluno) < 2;
