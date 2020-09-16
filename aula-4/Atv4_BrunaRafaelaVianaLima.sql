e421866fa652def3dc73957510c13bd3024198cc
brunarafaela <brunarafaelav@outlook.com>
2020-09-15 22:54:48 -0300

    Create Atv4_BrunaRafaelaVianaLima.sql

---
+++ aula-4/Atv4_BrunaRafaelaVianaLima.sql
@@ -0,0 +1,14 @@
+/*****************************
+ Atividade 14  
+1 Montar o script em SQL para a criacao das tabelas QUE NÃO FORAM IMPLEMENTADAS AINDA EM AULA, 
+conforme o esquema de relacao gerado a partir da Atividade 0 acima, no SGBD Oracle com as seguintes caracteristicas: 
+a) Codigo da matricula eh uma sequencia comecando em 2000 ;
+b) Acoes referenciais ON DELETE .
+ **************************/
+
+--1) Reescrever a consulta 17 da aula com a sintaxe do INNER JOIN;
+--2) Mostrar para cada curso a média de duração das turmas em dias;
+--3) Mostrar a quantidade de matriculados por curso, mês (use a data da matrícula) e sexo com idade superior a 21 anos; 4) Mostrar os cursos que utilizam mais de 3 softwares (nome do curso);
+--5) Mostrar o nome do curso e a turma para os cursos da empresa Cisco que tiveram mais de R$ 2mil de arrecadação por turma.
+--6) Mostrar para cada empresa certificadora o valor total arrecadado e a média com as matrículas nos seus cursos, para cada mês, e o número de matriculados mas desde que ultrapassem mais de 50 por mês.
+--7) Mostrar para cada aluno (nome) a quantidade de aulas cursadas em cada curso matriculado.
