ef17514056c036165c491be04867e95a9f31888d
Bruna Rafaela <Bruna@MacBook-Pro-de-Bruna.local>
2020-10-20 14:25:41 -0300

    Aula6

---
+++ aula-6/Atv6_BrunaRafaelaVianaLima.sql
@@ -0,0 +1,14 @@
+/*Atividade 6 - Banco de Dados Objeto-Relacional : Com as instruções SQL para tipos e tabelas tipadas no Objeto Relacional
+a) Defina um tipo para turma e a respectiva tabela tipada, com uma referência para o objeto curso. Popule com duas linhas a tabela
+tipada.
+b) Defina um tipo para empresa : nome e nacionalidade e a respectiva tabela tipada. Popule com três empresas (Ex: Oracle, Cisco e
+Microsoft).
+c) Defina um tipo para software com os mesmos atributos que o relacional mas agora com uma referência para o objeto Fabricante.
+Popule com uma linha para cada fabricante criado em b) para a tabela tipada de software. */
+
+
+/*a) Defina um tipo para turma e a respectiva tabela tipada, com uma referência para o objeto curso. Popule com duas linhas a tabela tipada.*/
+
+
+/*Turma(num_turma(PK),id_curso(PK)(FK),dt_inicio,dt_termino,vagas,horario_aula,vl_curso,instrutor,situacao_turma)*/
+
--- aula-6/certificacoesSGBD.sql
+++ aula-6/certificacoesSGBD.sql
@@ -1219,5 +1219,125 @@ continua...
 */
 
 
+/* parametros de configuracao da sessao */
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY HH24:MI:SS';
+ALTER SESSION SET NLS_LANGUAGE = PORTUGUESE;
+SELECT SESSIONTIMEZONE, CURRENT_TIMESTAMP FROM DUAL;
 
 
+--vetor para fone
+DROP TYPE tipofone FORCE;
+CREATE OR REPLACE TYPE tipofone AS VARRAY(5) OF INTEGER;
+DESC tipofone;
+
+--definindo um tipo para endereço
+DROP TYPE tipoendereco FORCE;
+CREATE OR REPLACE TYPE tipoendereco AS OBJECT
+(tipo_logradouro CHAR(12),
+logradouro VARCHAR(40),
+numero SMALLINT,
+complemento CHAR(10),
+bairro VARCHAR2(25),
+cidade VARCHAR2(30),
+UF CHAR(2),
+email VARCHAR2(32),
+fones tipofone)
+NOT FINAL; --permite ter mais derivadas
+
+
+--tipo para aluno
+DROP TYPE tipoaluno FORCE;
+CREATE OR REPLACE TYPE tipoaluno AS OBJECT
+(id_aluno INTEGER,
+nome_aluno VARCHAR2(50),
+sexo_aluno CHAR(1),
+dt_nascto_aluno DATE,
+cpf_aluno NUMBER(11),
+end_aluno tipoendereco,
+login CHAR(10),
+senha CHAR(6))
+FINAL;
+
+
+--tabela tipada para aluno
+DROP TABLE or_aluno CASCADE CONSTRAINTS;
+CREATE TABLE or_aluno OF tipoaluno
+(PRIMARY KEY (id_aluno),
+nome_aluno NOT NULL,
+CHECK (sexo_aluno IN ('M', 'F')),
+UNIQUE (cpf_aluno),
+UNIQUE (login))
+OBJECT IDENTIFIER IS SYSTEM GENERATED;
+
+--populando aluno
+INSERT INTO or_aluno VALUES (1, 'Sebastiao Malacale', 'M', current_date- INTERVAL '20' YEAR, 1234, 
+	tipoendereco ('Rua', 'Vergueiro', 7000, null, 'Ipiranga', 'São Paulo', 'SP', 'macale20@gmail.com', 
+	             tipofone(11912345678, 119987654321,11974638293)), 'tiaomaca', 'tiao20');
+
+SELECT * FROM or_aluno;
+
+SELECT a.nome_aluno, a.login, a.end_aluno.logradouro, a.end_aluno.numero
+FROM or_aluno a;
+
+SELECT a.nome_aluno, a.login, a.end_aluno.logradouro, a.end_aluno.fones
+FROM or_aluno a; --erro
+
+SELECT a.nome_aluno, a.login, a.end_aluno.logradouro, f.*
+FROM or_aluno a, TABLE (a.end_aluno.fones) f; --table serve para abrira coleção, desanina o que está aninhado
+
+--tipo para cetificação
+DROP TYPE tipocertificacao FORCE;
+CREATE OR REPLACE TYPE tipocertificacao AS OBJECT(
+id_cert CHAR(6),
+nome_cert VARCHAR2(50),
+carga_hora_cert SMALLINT,
+tempo_maximo SMALLINT,
+empresa_certificadora VARCHAR2(40));
+
+--tabela tipada para certificacao
+DROP TABLE or_certificacao CASCADE CONSTRAINTS;
+CREATE TABLE or_certificacao OF tipocertificacao
+(PRIMARY KEY (id_cert),
+nome_cert NOT NULL,
+CHECK(carga_hora_cert > 0))
+OBJECT IDENTIFIER IS SYSTEM GENERATED;
+
+--populanco certificacao
+INSERT INTO or_certificacao VALUES ('OCA', 'Oracle Certified Associated', 240, 180, 'Oracle Corporation');
+--tipo para curso --falta aulas programadas e softwares usados, serao tabelas aminhadas
+DROP TYPE tipocurso FORCE;
+CREATE OR REPLACE TYPE tipocurso AS OBJECT
+(id_curso CHAR(6),
+nome_curso VARCHAR2(50),
+carga_hora_curso SMALLINT,
+nota_corte_curso NUMBER(4,2),
+qtde_aulas SMALLINT,
+sequencia SMALLINT,
+certficacao REF tipocertificacao);
+
+--tabela tipada para curso
+DROP TABLE or_curso CASCADE CONSTRAINTS;
+CREATE TABLE or_curso OF tipocurso
+(PRIMARY KEY (id_curso),
+nome_curso NOT NULL,
+CHECK (nota_corte_curso >0));
+
+--buscando o OID de um objeto
+SELECT REF(ce) FROM or_certificacao ce WHERE ce.id_cert = 'OCA';
+
+--populando curso
+INSERT INTO or_curso VALUES ('SQL', 'Fundamentos de SQL', 40, 75, 10, 1, 
+(SELECT REF(ce) FROM or_certificacao ce WHERE ce.id_cert = 'OCA') );
+
+--relacionando curso e certificação
+SELECT c.nome_curso, c.carga_hora_curso, c.certificacao.nome_cert
+FROM or_curso c;
+
+--tipo para aula
+DROP TYPE tipoaula_programada FORCE;
+CREATE OR REPLACE TYPE tipoaula_programada AS OBJECT
+(numero SMALLINT,
+conteudo_previsto VARCHAR2(100),
+atividade_prevista VARCHAR2(50),
+material VARCHAR2 (40),
+arquivo_material VARCHAR2(50));
